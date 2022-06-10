fe8d0a33c28d1aa2faa00f9be9029cd2805fe611
Enes <66560757+devenes@users.noreply.github.com>
2022-06-10 16:10:21 +0300

    Update getItems.spec.js

--- spec/routes/getItems.spec.js
+++ spec/routes/getItems.spec.js
@@ -1,6 +1,8 @@
 const db = require('../../src/persistence');
 const getItems = require('../../src/routes/getItems');
-const ITEMS = [{ id: 12345 }];
+const ITEMS = [{
+    id: 12345
+}];
 
 jest.mock('../../src/persistence', () => ({
     getItems: jest.fn(),
@@ -8,7 +10,9 @@ jest.mock('../../src/persistence', () => ({
 
 test('it gets items correctly', async () => {
     const req = {};
-    const res = { send: jest.fn() };
+    const res = {
+        send: jest.fn()
+    };
     db.getItems.mockReturnValue(Promise.resolve(ITEMS));
 
     await getItems(req, res);
